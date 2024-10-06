32d373511d75e47bbefb9d6b53b3d87a863e3129
shaurya2014kira <166789175+shaurya2014kira@users.noreply.github.com>
2024-10-06 14:59:02 +0530

    added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin:["http://localhost:5173","https://product-client-repo.vercel.app"],
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
 }));
